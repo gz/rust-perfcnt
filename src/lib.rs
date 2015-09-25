7b12d881f90c47720270faacc9dbc0ffe81ceeba
Gerd Zellweger <mail@gerdzellweger.com>
2015-09-25 21:20:23 +0200

    Initial import.

---
+++ src/lib.rs
@@ -0,0 +1,42 @@
+#![feature(negate_unsigned)]
+
+extern crate libc;
+#[macro_use]
+extern crate syscall;
+#[macro_use]
+extern crate bitflags;
+
+
+mod linux;
+use std::mem;
+
+#[test]
+fn basic_perfcnt_linux() {
+
+    let mut hw_event: linux::perf_event::perf_event_attr = Default::default();
+    hw_event._type = linux::perf_event::PERF_TYPE_HARDWARE;
+    hw_event.size = mem::size_of::<linux::perf_event::perf_event_attr>() as u32;
+    hw_event.config = linux::perf_event::PERF_COUNT_HW_INSTRUCTIONS as u64;
+    hw_event.settings = linux::perf_event::EVENT_ATTR_DISABLED | linux::perf_event::EVENT_ATTR_EXCLUDE_KERNEL | linux::perf_event::EVENT_ATTR_EXCLUDE_HV;
+
+    let pid = 0;
+    let cpu = -1;
+    let group_fd = -1;
+    let flags = 0;
+
+    let fd = linux::perf_event_open(hw_event, pid, cpu, group_fd, flags) as ::libc::c_int;
+    if fd < 0 {
+        println!("Error opening leader {:?}", fd);
+    }
+
+    let i1 = linux::ioctl(fd, linux::perf_event::PERF_EVENT_IOC_RESET, 0);
+    let i2 = linux::ioctl(fd, linux::perf_event::PERF_EVENT_IOC_ENABLE, 0);
+    println!("{:?}", fd);
+    let i3 = linux::ioctl(fd, linux::perf_event::PERF_EVENT_IOC_DISABLE, 0);
+    println!("{:?} {:?} {:?}", i1, i2, i3);
+
+    let c = linux::read_counter(fd);
+    println!("Counter is {:?}", c.unwrap());
+
+
+}
---
+++ src/linux/mod.rs
@@ -0,0 +1,39 @@
+use std::fs::File;
+use std::os::unix::io::FromRawFd;
+use std::io::Read;
+use std::io;
+use std::mem;
+use std::slice::from_raw_parts_mut;
+
+
+#[allow(dead_code, non_camel_case_types)]
+mod hw_breakpoint;
+#[allow(dead_code, non_camel_case_types)]
+pub mod perf_event;
+
+
+pub fn perf_event_open(hw_event: perf_event::perf_event_attr,
+                       pid: perf_event::__kernel_pid_t,
+                       cpu:  ::libc::c_int,
+                       group_fd:  ::libc::c_int,
+                       flags:  ::libc::c_int) -> isize {
+    unsafe {
+        syscall!(PERF_EVENT_OPEN, &hw_event as *const perf_event::perf_event_attr as usize, pid, cpu, group_fd, flags) as isize
+    }
+}
+
+pub fn ioctl(fd: ::libc::c_int, request: u64, value: ::libc::c_int) -> isize {
+    unsafe {
+        syscall!(IOCTL, fd, request, value) as isize
+    }
+}
+
+pub fn read_counter(fd: ::libc::c_int) -> Result<u64, io::Error> {
+    unsafe {
+        let mut f = File::from_raw_fd(fd);
+        let mut value: [u8; 8] = Default::default();
+        let result = try!(f.read(&mut value));
+
+        Ok(mem::transmute::<[u8; 8], u64>(value))
+    }
+}
