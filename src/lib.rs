8008e32578133641d445f9d3f10583fa53e9546e
Gerd Zellweger <mail@gerdzellweger.com>
2015-09-28 18:27:43 +0200

    Clean-up, refactor, remove warnings.

--- src/lib.rs
+++ src/lib.rs
@@ -6,40 +6,28 @@ extern crate x86;
 #[macro_use]
 extern crate bitflags;
 
+#[cfg(target_os="linux")] #[path="linux/mod.rs"]
+pub mod arch;
 
-mod linux;
-use std::mem;
+
+
+/// Abstract trait to control performance counters.
+trait PerfCounterControl {
+    fn new() -> arch::PerfCounter;
+    fn reset(&self);
+    fn start(&self);
+    fn stop(&self);
+    fn read(&self) -> u64;
+}
 
 #[test]
-fn basic_perfcnt_linux() {
+fn basic_perfcnt() {
+    let pc = arch::PerfCounter::new();
 
-    let mut hw_event: linux::perf_event::perf_event_attr = Default::default();
-    hw_event._type = linux::perf_event::PERF_TYPE_HARDWARE;
-    hw_event.size = mem::size_of::<linux::perf_event::perf_event_attr>() as u32;
-    hw_event.config = linux::perf_event::PERF_COUNT_HW_INSTRUCTIONS as u64;
-    hw_event.settings = linux::perf_event::EVENT_ATTR_DISABLED | linux::perf_event::EVENT_ATTR_EXCLUDE_KERNEL | linux::perf_event::EVENT_ATTR_EXCLUDE_HV;
+    pc.reset();
+    pc.start();
+    println!("test");
+    pc.stop();
 
-    let pid = 0;
-    let cpu = -1;
-    let group_fd = -1;
-    let flags = 0;
-
-    let fd = linux::perf_event_open(hw_event, pid, cpu, group_fd, flags) as ::libc::c_int;
-    if fd < 0 {
-        println!("Error opening leader {:?}", fd);
-    }
-
-    let i1 = linux::ioctl(fd, linux::perf_event::PERF_EVENT_IOC_RESET, 0);
-    let i2 = linux::ioctl(fd, linux::perf_event::PERF_EVENT_IOC_ENABLE, 0);
-    println!("{:?}", fd);
-    let i3 = linux::ioctl(fd, linux::perf_event::PERF_EVENT_IOC_DISABLE, 0);
-    println!("{:?} {:?} {:?}", i1, i2, i3);
-
-    let c = linux::read_counter(fd);
-    match c {
-        Ok(cnt) => println!("Counter is {:?}", cnt),
-        Err(e) => println!("Can't read counter: {:?}", e),
-    }
-
-
+    println!("{:?}", pc.read());
 }
--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -3,19 +3,18 @@ use std::os::unix::io::FromRawFd;
 use std::io::Read;
 use std::io;
 use std::mem;
-use std::slice::from_raw_parts_mut;
-
 
 #[allow(dead_code, non_camel_case_types)]
 mod hw_breakpoint;
 #[allow(dead_code, non_camel_case_types)]
 pub mod perf_event;
 
+use ::PerfCounterControl;
 
-pub const IOCTL: usize = 16;
-pub const PERF_EVENT_OPEN: usize = 298;
+const IOCTL: usize = 16;
+const PERF_EVENT_OPEN: usize = 298;
 
-pub fn perf_event_open(hw_event: perf_event::perf_event_attr,
+fn perf_event_open(hw_event: perf_event::perf_event_attr,
                        pid: perf_event::__kernel_pid_t,
                        cpu:  ::libc::c_int,
                        group_fd:  ::libc::c_int,
@@ -25,18 +24,67 @@ pub fn perf_event_open(hw_event: perf_event::perf_event_attr,
     }
 }
 
-pub fn ioctl(fd: ::libc::c_int, request: u64, value: ::libc::c_int) -> isize {
+fn ioctl(fd: ::libc::c_int, request: u64, value: ::libc::c_int) -> isize {
     unsafe {
         syscall!(IOCTL, fd, request, value) as isize
     }
 }
 
-pub fn read_counter(fd: ::libc::c_int) -> Result<u64, io::Error> {
+fn read_counter(fd: ::libc::c_int) -> Result<u64, io::Error> {
     unsafe {
         let mut f = File::from_raw_fd(fd);
         let mut value: [u8; 8] = Default::default();
-        let result = try!(f.read(&mut value));
-
+        try!(f.read(&mut value));
         Ok(mem::transmute::<[u8; 8], u64>(value))
+    }
+}
+
+pub struct PerfCounter {
+    fd: ::libc::c_int
+}
+
+impl PerfCounterControl for PerfCounter {
+
+    fn new() -> PerfCounter {
+        let mut hw_event: perf_event::perf_event_attr = Default::default();
+        hw_event._type = perf_event::PERF_TYPE_HARDWARE;
+        hw_event.size = mem::size_of::<perf_event::perf_event_attr>() as u32;
+        hw_event.config = perf_event::PERF_COUNT_HW_INSTRUCTIONS as u64;
+        hw_event.settings = perf_event::EVENT_ATTR_DISABLED | perf_event::EVENT_ATTR_EXCLUDE_KERNEL | perf_event::EVENT_ATTR_EXCLUDE_HV;
+
+        let pid = 0;
+        let cpu = -1;
+        let group_fd = -1;
+        let flags = 0;
+
+        let fd = perf_event_open(hw_event, pid, cpu, group_fd, flags) as ::libc::c_int;
+        if fd < 0 {
+            println!("Error opening leader {:?}", fd);
+        }
+
+        PerfCounter { fd: fd }
+    }
+
+    fn reset(&self) {
+        let ret = ioctl(self.fd, perf_event::PERF_EVENT_IOC_RESET, 0);
+        assert!(ret == 0);
+    }
+
+    fn start(&self) {
+        let ret = ioctl(self.fd, perf_event::PERF_EVENT_IOC_ENABLE, 0);
+        assert!(ret == 0);
+    }
+
+    fn stop(&self) {
+        let ret = ioctl(self.fd, perf_event::PERF_EVENT_IOC_DISABLE, 0);
+        assert!(ret == 0);
+    }
+
+    fn read(&self) -> u64 {
+        let c = read_counter(self.fd);
+        match c {
+            Ok(cnt) => cnt,
+            _ => 0,
+        }
     }
 }
