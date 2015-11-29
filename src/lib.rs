d6ffe4076ec7de45007c76b0232f88c48d380c06
Gerd Zellweger <mail@gerdzellweger.com>
2015-11-29 11:25:35 +0100

    Remove performance counter data.

--- src/lib.rs
+++ src/lib.rs
@@ -1,16 +1,14 @@
 #![feature(negate_unsigned)]
-extern crate phf;
-
 extern crate libc;
 #[macro_use]
 extern crate x86;
 #[macro_use]
 extern crate bitflags;
 
-pub mod intel;
-
 #[cfg(target_os="linux")] #[path="linux/mod.rs"]
 pub mod arch;
+
+use x86::perfcnt;
 
 /// Abstract trait to control performance counters.
 trait PerfCounterControl {
@@ -23,14 +21,14 @@ trait PerfCounterControl {
 
 // #[test]
 fn list_mine() {
-    for counter in intel::available_counters() {
+    for counter in x86::perfcnt::core_counters() {
         println!("{:?}", counter);
     }
 }
 
 #[test]
 fn list_them() {
-    for counter in intel::counters::HASWELLX_CORE.values() {
+    for counter in x86::perfcnt::intel::counters::COUNTER_MAP.get("GenuineIntel-6-3F-core") {
         println!("{:?}", counter);
     }
 }
