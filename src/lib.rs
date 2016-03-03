e04807447b6f20c7cdb13b694f1bda32416d4974
Gerd Zellweger <mail@gerdzellweger.com>
2016-03-03 20:59:23 +0100

    Add example, cleanup naming.
    Signed-off-by: Gerd Zellweger <mail@gerdzellweger.com>

--- src/lib.rs
+++ src/lib.rs
@@ -7,45 +7,13 @@ extern crate bitflags;
 
 mod linux;
 
-pub use linux::{PerfCounterBuilder, PerfCounter};
+pub use linux::{PerfCounterBuilderLinux, PerfCounter};
 use std::io;
 
-
 /// Abstract trait to control performance counters.
-trait AbstractPerfCounter {
+pub trait AbstractPerfCounter {
     fn reset(&self) -> Result<(), io::Error>;
     fn start(&self) -> Result<(), io::Error>;
     fn stop(&self) -> Result<(), io::Error>;
     fn read(&self) -> Result<u64, io::Error>;
-}
-
-
-#[test]
-fn list_mine() {
-    for counter in x86::perfcnt::core_counters() {
-        //println!("{:?}", counter);
-    }
-}
-
-#[test]
-fn list_them() {
-    for counter in x86::perfcnt::core_counters() {
-        //println!("{:?}", counter);
-    }
-}
-
-#[test]
-fn basic_perfcnt() {
-    let counter = x86::perfcnt::core_counters().unwrap().get("BR_INST_RETIRED.ALL_BRANCHES").unwrap();
-    println!("{:?}", counter.brief_description);
-    let mut pb = PerfCounterBuilder::new();
-    pb.from_raw_intel_hw_counter(counter);
-    let pc: PerfCounter = pb.finish();
-
-    pc.reset().expect("Can not reset the counter");
-    pc.start().expect("Can not start the counter");
-    println!("");
-    pc.stop().expect("Can not stop the counter");;
-
-    println!("{:?}", pc.read().expect("Can not read counter"));
 }
--- src/linux/mod.rs
+++ src/linux/mod.rs
@@ -43,7 +43,7 @@ fn read_counter(fd: ::libc::c_int) -> Result<u64, io::Error> {
     }
 }
 
-pub struct PerfCounterBuilder {
+pub struct PerfCounterBuilderLinux {
     group: isize,
     pid: pid_t,
     cpu: isize,
@@ -51,9 +51,9 @@ pub struct PerfCounterBuilder {
     attrs: perf_event::perf_event_attr,
 }
 
-impl Default for PerfCounterBuilder {
-    fn default () -> PerfCounterBuilder {
-        PerfCounterBuilder {
+impl Default for PerfCounterBuilderLinux {
+    fn default () -> PerfCounterBuilderLinux {
+        PerfCounterBuilderLinux {
             group: -1,
             pid: 0,
             cpu: -1,
@@ -63,13 +63,13 @@ impl Default for PerfCounterBuilder {
     }
 }
 
-impl PerfCounterBuilder {
+impl PerfCounterBuilderLinux {
 
-    pub fn new() -> PerfCounterBuilder {
+    pub fn new() -> PerfCounterBuilderLinux {
         Default::default()
     }
 
-    pub fn set_group<'a>(&'a mut self, group_fd: isize) -> &'a mut PerfCounterBuilder {
+    pub fn set_group<'a>(&'a mut self, group_fd: isize) -> &'a mut PerfCounterBuilderLinux {
         self.group = group_fd;
         self
     }
@@ -77,7 +77,7 @@ impl PerfCounterBuilder {
     /// Sets PERF_FLAG_FD_OUTPUT
     ///
     /// This flag re-routes the output from an event to the group leader.
-    pub fn set_flag_fd_output<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn set_flag_fd_output<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.flags |= 0x0; //PERF_FLAG_FD_OUTPUT;
         self
     }
@@ -89,136 +89,136 @@ impl PerfCounterBuilder {
     /// finer grain control (CPUs, memory, etc.).   In  this  mode,  the
     /// event  is  measured  only if the thread running on the monitored
     /// CPU belongs to the designated container (cgroup).
-    pub fn set_flag_pid_cgroup<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn set_flag_pid_cgroup<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.flags |= 0x0; //PERF_FLAG_PID_CGROUP;
         self
     }
 
-    pub fn set_sample_period<'a>(&'a mut self, period: u64) -> &'a mut PerfCounterBuilder {
+    pub fn set_sample_period<'a>(&'a mut self, period: u64) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.sample_period_freq = period;
         self
     }
 
-    pub fn set_sample_frequency<'a>(&'a mut self, frequency: u64) -> &'a mut PerfCounterBuilder {
+    pub fn set_sample_frequency<'a>(&'a mut self, frequency: u64) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.sample_period_freq = frequency;
         self.attrs.settings.insert(perf_event::EVENT_ATTR_FREQ);
         self
     }
 
     /// The counter starts out disabled.
-    pub fn disable<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn disable<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_DISABLED);
         self
     }
 
     /// This counter should count events of child tasks as well as the task specified.
-    pub fn inherit<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn inherit<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_INHERIT);
         self
     }
 
     /// The pinned bit specifies that the counter should always be on the CPU if at all possible.
     /// It applies only to  hardware counters and only to group leaders.
-    pub fn pinned<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn pinned<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_PINNED);
         self
     }
 
     /// The counter is exclusive i.e., when this counter's group is on the CPU,
     /// it should be the only group using the CPU's counters.
-    pub fn exclusive<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn exclusive<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_EXCLUSIVE);
         self
     }
 
     /// The counter excludes events that happen in user space.
-    pub fn exclude_user<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn exclude_user<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_EXCLUDE_USER);
         self
     }
 
     /// The counter excludes events that happen in the kernel.
-    pub fn exclude_kernel<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn exclude_kernel<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_EXCLUDE_KERNEL);
         self
     }
 
     /// The counter excludes events that happen in the hypervisor.
-    pub fn exclude_hv<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn exclude_hv<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_EXCLUDE_HV);
         self
     }
 
     /// The counter doesn't count when the CPU is idle.
-    pub fn exclude_idle<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn exclude_idle<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_EXCLUDE_IDLE);
         self
     }
 
     /// Enables recording of exec mmap events.
-    pub fn enable_mmap<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn enable_mmap<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_MMAP);
         self
     }
 
     /// The counter will save event counts on context switch for inherited tasks.
     /// This is meaningful only if the inherit field is set.
-    pub fn inherit_stat<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn inherit_stat<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_INHERIT_STAT);
         self
     }
 
     /// The counter is automatically enabled after a call to exec.
-    pub fn enable_on_exec<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn enable_on_exec<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_ENABLE_ON_EXEC);
         self
     }
 
     /// fork/exit notifications are included in the ring buffer.
-    pub fn enable_task_notification<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn enable_task_notification<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_TASK);
         self
     }
 
     /// The counter has  a  sampling  interrupt happen when we cross the wakeup_watermark
     /// boundary.  Otherwise interrupts happen after wakeup_events samples.
-    pub fn enable_watermark<'a>(&'a mut self, watermark_events: u32) -> &'a mut PerfCounterBuilder {
+    pub fn enable_watermark<'a>(&'a mut self, watermark_events: u32) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_WATERMARK);
         self.attrs.wakeup_events_watermark = watermark_events;
         self
     }
 
     /// Sampled IP counter can have arbitrary skid.
-    pub fn set_ip_sample_arbitrary_skid<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn set_ip_sample_arbitrary_skid<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_SAMPLE_IP_ARBITRARY_SKID);
         self
     }
 
     /// Sampled IP counter requested to have constant skid.
-    pub fn set_ip_sample_constant_skid<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn set_ip_sample_constant_skid<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_SAMPLE_IP_CONSTANT_SKID);
         self
     }
 
     /// Sampled IP counter requested to have 0 skid.
-    pub fn set_ip_sample_req_zero_skid<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn set_ip_sample_req_zero_skid<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_SAMPLE_IP_REQ_ZERO_SKID);
         self
     }
 
     /// The counterpart of enable_mmap, but enables including data mmap events in the ring-buffer.
-    pub fn enable_mmap_data<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn enable_mmap_data<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_MMAP_DATA);
         self
     }
 
     /// Sampled IP counter must have 0 skid.
-    pub fn set_ip_sample_zero_skid<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn set_ip_sample_zero_skid<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.attrs.settings.insert(perf_event::EVENT_ATTR_SAMPLE_IP_ZERO_SKID);
         self
     }
 
-    pub fn from_raw_intel_hw_counter<'a>(&'a mut self, counter: &IntelPerformanceCounterDescription) -> &'a mut PerfCounterBuilder {
+    pub fn from_raw_intel_hw_counter<'a>(&'a mut self, counter: &IntelPerformanceCounterDescription) -> &'a mut PerfCounterBuilderLinux {
         let mut config: u64 = 0;
 
         match counter.event_code {
@@ -246,34 +246,35 @@ impl PerfCounterBuilder {
         self
     }
 
-    pub fn for_all_pids<'a>(&'a mut self) ->  &'a mut PerfCounterBuilder {
+    pub fn for_all_pids<'a>(&'a mut self) ->  &'a mut PerfCounterBuilderLinux {
         self.pid = -1;
         self
     }
 
-    pub fn for_pid<'a>(&'a mut self, pid: i32) -> &'a mut PerfCounterBuilder {
+    pub fn for_pid<'a>(&'a mut self, pid: i32) -> &'a mut PerfCounterBuilderLinux {
         self.pid = pid;
         self
     }
 
-    pub fn on_cpu<'a>(&'a mut self, cpu: isize) -> &'a mut PerfCounterBuilder {
+    pub fn on_cpu<'a>(&'a mut self, cpu: isize) -> &'a mut PerfCounterBuilderLinux {
         self.cpu = cpu;
         self
     }
 
-    pub fn on_all_cpus<'a>(&'a mut self) -> &'a mut PerfCounterBuilder {
+    pub fn on_all_cpus<'a>(&'a mut self) -> &'a mut PerfCounterBuilderLinux {
         self.cpu = -1;
         self
     }
 
-    pub fn finish(&self) -> PerfCounter {
+    pub fn finish(&self) -> Result<PerfCounter, io::Error> {
         let flags = 0;
         let fd = perf_event_open(self.attrs, self.pid, self.cpu as i32, self.group as i32, flags) as ::libc::c_int;
         if fd < 0 {
-            println!("Error opening leader {:?}", fd);
+            // TODO: Our syscall invocation probably does not set errno?
+            return Err(Error::last_os_error());
         }
 
-        PerfCounter { fd: fd }
+        Ok(PerfCounter { fd: fd })
     }
 }
 
