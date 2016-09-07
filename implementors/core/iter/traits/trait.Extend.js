6a2aca7fb04ff68803d4a05f4adea2e500fa6d52
Gerd Zellweger <mail@gerdzellweger.com>
2016-09-07 17:19:29 +0100

    Update documentation

---
+++ implementors/core/iter/traits/trait.Extend.js
@@ -0,0 +1,10 @@
+(function() {var implementors = {};
+implementors["perfcnt"] = ["impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html' title='core::iter::traits::Extend'>Extend</a>&lt;<a class='struct' href='perfcnt/linux/perf_format/struct.ReadFormatFlags.html' title='perfcnt::linux::perf_format::ReadFormatFlags'>ReadFormatFlags</a>&gt; for <a class='struct' href='perfcnt/linux/perf_format/struct.ReadFormatFlags.html' title='perfcnt::linux::perf_format::ReadFormatFlags'>ReadFormatFlags</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html' title='core::iter::traits::Extend'>Extend</a>&lt;<a class='struct' href='perfcnt/linux/perf_format/struct.SampleFormatFlags.html' title='perfcnt::linux::perf_format::SampleFormatFlags'>SampleFormatFlags</a>&gt; for <a class='struct' href='perfcnt/linux/perf_format/struct.SampleFormatFlags.html' title='perfcnt::linux::perf_format::SampleFormatFlags'>SampleFormatFlags</a>","impl <a class='trait' href='https://doc.rust-lang.org/nightly/core/iter/traits/trait.Extend.html' title='core::iter::traits::Extend'>Extend</a>&lt;<a class='struct' href='perfcnt/linux/perf_format/struct.EventAttrFlags.html' title='perfcnt::linux::perf_format::EventAttrFlags'>EventAttrFlags</a>&gt; for <a class='struct' href='perfcnt/linux/perf_format/struct.EventAttrFlags.html' title='perfcnt::linux::perf_format::EventAttrFlags'>EventAttrFlags</a>",];
+
+            if (window.register_implementors) {
+                window.register_implementors(implementors);
+            } else {
+                window.pending_implementors = implementors;
+            }
+        
+})()
